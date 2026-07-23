e92cec55104b5e75623c907d2dbf8ad5fbeddbee
Hanh Huynh Huu <hanh425@gmail.com>
2026-07-23 22:53:44 +0200

    fix: use add_bundle_zsa for ZSA bundle verification in tx_extractor

--- pczt/src/roles/tx_extractor/orchard.rs
+++ pczt/src/roles/tx_extractor/orchard.rs
@@ -8,15 +8,16 @@ pub(super) fn verify_bundle(
     sighash: [u8; 32],
     consensus_branch_id: BranchId,
 ) -> Result<(), OrchardError> {
+    let is_zsa = consensus_branch_id == BranchId::Nu7;
     match orchard_vk {
-        Some(vk) => verify_bundle_with_key(bundle, vk, sighash),
+        Some(vk) => verify_bundle_with_key(bundle, vk, sighash, is_zsa),
         None => {
-            let vk = if consensus_branch_id == BranchId::Nu7 {
+            let vk = if is_zsa {
                 VerifyingKey::build_zsa()
             } else {
                 VerifyingKey::build(bundle.bundle_version().circuit_version())
             };
-            verify_bundle_with_key(bundle, &vk, sighash)
+            verify_bundle_with_key(bundle, &vk, sighash, is_zsa)
         }
     }
 }
@@ -25,11 +26,18 @@ fn verify_bundle_with_key(
     bundle: &Bundle<Authorized, ZatBalance>,
     vk: &VerifyingKey,
     sighash: [u8; 32],
+    is_zsa: bool,
 ) -> Result<(), OrchardError> {
     let mut validator = orchard::bundle::BatchValidator::new(vk);
-    validator
-        .add_bundle(bundle, sighash)
-        .map_err(|_| OrchardError::InvalidProof)?;
+    if is_zsa {
+        validator
+            .add_bundle_zsa(bundle, sighash)
+            .map_err(|_| OrchardError::InvalidProof)?;
+    } else {
+        validator
+            .add_bundle(bundle, sighash)
+            .map_err(|_| OrchardError::InvalidProof)?;
+    }
 
     if validator.validate(OsRng) {
         Ok(())
